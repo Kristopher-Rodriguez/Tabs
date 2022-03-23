7b0ab3e523a12d6e4dddac892af48a6fbba0a00e
Kris Rodriguez <k.rod.518@gmail.com>
2022-03-23 12:04:17 -0500

    Solution for Tabs Assignment

---
+++ src/components/Results.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Results = (props) => {
+  const { allTabs, currentTabIndex } = props;
+
+  return <div className="results">{allTabs[currentTabIndex].content}</div>;
+};
+
+export default Results;
